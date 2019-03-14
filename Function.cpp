d4fe71520c4eaca18767607db70026f3c4216f20
t3bol90 <43676770+t3bol90@users.noreply.github.com>
2019-03-14 21:49:47 +0700

    Refactor code || Write class CLASS || Merge code

--- Function.cpp
+++ Function.cpp
@@ -1,147 +1,398 @@
 #include "Header.h"
-void menu()
-{
-	char one = getchar();
-	system("cls");
-	cout << "1.Log in" << endl;
-	cout << "2.Exit" << endl;
-	cout << "\t\tEnter number : ";
-	int ans;
-	cin >> ans;
-	switch (ans)
-	{
-	case 1:
-	{
-		system("cls");
-		char id[50];
-		char password[50];
-		cout << "Please enter your id : ";
-		cin >> id;
-		cout << "\nPlease enter your password : ";
-		cin >> password;
-		int l1 = strlen(id);
-		int l2 = strlen(password);
-		while (l1 >= 10 || l2 >= 10 || (l1 >= 10 && l2 >= 10))
-		{
-			system("cls");
-			cout << "Your account or password has a maximum of 9 characters. Please try again!" << endl;
 
-			cout << "Please enter your id : ";
-
-			cin >> id;
-			cout << "\nPlease enter your password : ";
-
-			cin >> password;
-			l1 = strlen(id);
-			l2 = strlen(password);
-		}
-		if (id[0] == 's')
-		{
-			system("cls");
-			staff(id);
-		}
-		else if (id[0] == 'l')
-		{
-			system("cls");
-			lecture(id);
-		}
-		else if (id[0] == '1')
-		{
-			system("cls");
-			student(id);
-		}
-	}break;
-	case 2:
-	{
-		system("cls");
-		cout << "Goodbye, Have a nice day!" << endl;
-		char two = getchar();
-		cout << "Press any key to exit!";
-	}break;
-	default:
-		break;
-	}
+bool loaddata(const char *path, STUDENT *&dest, int &n) {
+    ifstream fin;
+    fin.open(path);
+    if (!fin.is_open())
+        return false;
+    n = 0;
+    string firstline;
+    getline(fin, firstline);
+    while (!fin.eof()) {
+        STUDENT a;
+        string temp;
+        getline(fin, temp, ',');
+        a.No = stoi(temp);
+        getline(fin, temp, ',');
+        a.StudentID = stoi(temp);
+        getline(fin, a.lastname, ',');
+        getline(fin, a.firstname, ',');
+        getline(fin, temp, ',');
+        a.gender = temp != "Male";
+        getline(fin, temp, '/');
+        a.DoB.day = stoi(temp);
+        getline(fin, temp, '/');
+        a.DoB.month = stoi(temp);
+        getline(fin, temp, '\r');
+        a.DoB.year = stoi(temp);
+        getline(fin, temp, '\n');
+        init(dest, a, n);
+    }
+    fin.close();
+    return true;
 }
-void staff(char id[])
-{
-	system("cls");
-	cout << "Welcome " << id << endl;
-	cout << "\n1.Scoreboard" << endl;
-	cout << "\n2.Courses" << endl;
-	cout << "\n3.Class" << endl;
-	cout << "\n4.Attendance list" << endl;
-	cout << "\n5.Log out" << endl;
-	int ans1;
 
-	cout << "\nEnter your choice : ";
-	cin >> ans1;
-	switch (ans1)
-	{
-		case 1:
-		{
-			system("cls");
-			cout << "***Scoreboard***";
-			cout << "\nDo you want to continued?(y/n) ";
-			char b[10];
-			cin >> b;
-			if (strcmp(b, "y") == 0)
-				staff(id);
-			else
-				system("cls");
-			cout << "Goodbye, Have a nice day!" << endl;
-
-		}break;
-		case 2:
-		{
-			system("cls");
-			cout << "***Courses***";
-			char b[10];
-			cin >> b;
-			if (strcmp(b, "y") == 0)
-				staff(id);
-			else
-				system("cls");
-			cout << "Goodbye, Have a nice day!" << endl;
-		}break;
-		case 3:
-		{
-			system("cls");
-			cout << "***Class***";
-			char b[10];
-			cin >> b;
-			if (strcmp(b, "y") == 0)
-				staff(id);
-			else
-				system("cls");
-			cout << "Goodbye, Have a nice day!" << endl;
-
-		}break;
-		case 4:
-		{
-			system("cls");
-			cout << "***Attendance list***";
-			char b[10];
-			cin >> b;
-			if (strcmp(b, "y") == 0)
-				staff(id);
-			else
-				system("cls");
-			cout << "Goodbye, Have a nice day!" << endl;
-		}break;
-		case 5:
-		{
-			menu();
-		}
-
-	}
+void init(STUDENT *&dest, STUDENT a, int &n) {
+    auto *newdest = new STUDENT[n + 1];
+    for (size_t i = 0; i < n; i++) {
+        newdest[i] = dest[i];
+    }
+    delete[] dest;
+    newdest[n].No = a.No;
+    newdest[n].index = a.index;
+    newdest[n].firstname = a.firstname;
+    newdest[n].lastname = a.lastname;
+    newdest[n].gender = a.gender;
+    newdest[n].StudentID = a.StudentID;
+    newdest[n].DoB = a.DoB;
+    dest = newdest;
+    n++;
 }
-void lecture(char id[])
-{
-	cout << "Welcome " << id << endl;
+
+bool savedata(const char *path, STUDENT *&a, int n) {
+    ofstream fout;
+    fout.open(path);
+    if (!fout.is_open())
+        return false;
+    fout << n << endl;
+    for (int i = 0; i < n; i++) {
+        fout << a[i].StudentID << " " << a[i].lastname << " " << a[i].firstname << " " << a[i].gender << " "
+             << a[i].DoB.day << "/" << a[i].DoB.month << "/" << a[i].DoB.year << endl;
+    }
+    fout.close();
+    return true;
+    //No,StudentID,Lastname,Firstname,Gender,DoB
 
 }
-void student(char id[])
-{
-	cout << "Welcome " << id << endl;
 
+void STUDENT::output() {
+    cout << No << "\n\tSTUDENT: " << lastname << " " << firstname << " || ";
+    cout << "STUDENTID: " << StudentID << endl;
+}
+
+
+char firstMenu() {
+    system("cls");
+    cout << "1.Log in" << endl;
+    cout << "2.Exit" << endl;
+    cout << "\t\tEnter number : ";
+    char ans;
+    cin >> ans;
+    return ans;
+}
+
+
+void secondMenu(char userOption) {
+    switch (userOption) {
+        case '1': {
+            system("cls");
+            //input password id
+            string id;
+            string password;
+            inputIDPassword(id, password);
+            bool LandSPass = creatDefaultIP(DATA_STUDENT, STUDENT_PASSWORD);
+            if (LandSPass) {
+                if (id[0] == 's') {
+                    system("cls");
+                    secondStaffMenu(id);
+                } else if (id[0] == 'l') {
+                    system("cls");
+                    firstLectureMenu(id);
+                } else if (id[0] == '1') {
+                    system("cls");
+                    LandSPass = checkLoginStudent(id, password, STUDENT_PASSWORD);
+                    if (LandSPass) {
+                        firstStudentMenu(id);
+                    } else {
+                        cout << "Wrong id!! Please try again" << endl;
+                        secondMenu('1');
+                    }
+
+                } else {
+                    system("cls");
+                    system("pause");
+                    secondMenu('1');
+                }
+            }
+
+        }
+            break;
+        case '2': {
+            system("cls");
+            cout << "Goodbye, Have a nice day!" << endl;
+            cout << "Press any key to exit!";
+            char temp = getchar();
+            exit(0);
+        }
+            break;
+        default: {
+            system("cls");
+            cout << "Wrong input! Please try again" << endl;
+            system("pause");
+            secondMenu(firstMenu());
+        }
+    }
+}
+
+char firstStaffMenu(string id)
+{
+    system("cls");
+    cout << "Welcome " << id << endl;
+    cout << "\n[0] View profile info" << endl;
+    cout << "\n[1] Class" << endl;
+    cout << "\n[2] Courses" << endl;
+    cout << "\n[3] Scoreboard" << endl;
+    cout << "\n[4] Attendance list" << endl;
+    cout << "\n[5] Logout" << endl;
+    char userOption;
+    cout << "\nEnter your choice : ";
+    cin >> userOption;
+    return userOption;
+}
+void secondStaffMenu(string id) {
+    switch (firstStaffMenu(id)) {
+        case '0': {
+            system("cls");
+            cout << "***View profile info***";
+            systemContinue(id);
+        }
+        case '1': {
+            system("cls");
+            cout << "***Class***";
+            char staffOption;
+            staffMenuClass(id);
+            systemContinue(id);
+        }
+            break;
+        case '2': {
+            system("cls");
+            cout << "***Courses***";
+            systemContinue(id);
+        }
+            break;
+        case '3': {
+            system("cls");
+            cout << "***Scoreboard***";
+            systemContinue(id);
+        }
+            break;
+        case '4': {
+            system("cls");
+            cout << "***Attendance list***";
+            systemContinue(id);
+        }
+            break;
+        case '5': {
+            system("cls");
+            id = "";
+            secondMenu(firstMenu());
+        }
+
+    }
+}
+
+void firstLectureMenu(string id) {
+    cout << "Welcome " << id << endl;
+    systemContinue(id);
+}
+
+void firstStudentMenu(string id) {
+    cout << "Welcome " << id << endl;
+    system("pause");
+    systemContinue(id);
+}
+
+
+void systemContinue(string id) {
+    cout << "\nDo you want to continued?(yes/no) ";
+    string YesorNo;
+    cin >> YesorNo;
+    if (YesorNo == "yes") {
+        if (id[0] == 's') {
+            system("cls");
+            secondStaffMenu(id);
+        } else if (id[0] == 'l') {
+            system("cls");
+            firstLectureMenu(id);
+        } else if (id[0] == '1') {
+            system("cls");
+            firstStudentMenu(id);
+        }
+    } else {
+        system("cls");
+        cout << "Goodbye, Have a nice day!" << endl;
+        system("pause");
+        exit(0);
+    }
+}
+
+void staffMenuClass(string id) {
+    cout << "\n[1].Import students of a class." << endl;
+    cout << "\n[2].Add a new firstStudentMenu to a class." << endl;
+    cout << "\n[3].Edit an existing firstStudentMenu." << endl;
+    cout << "\n[4].Remove a firstStudentMenu." << endl;
+    cout << "\n[5].Change students from class A to class B." << endl;
+    cout << "\n[6].View list of classes." << endl;
+    cout << "\n[7].View list of firstStudentMenu in a class." << endl;
+    cout << "\n[8].Return" << endl;
+    cout << "Enter your choice : ";
+    char staffOption;
+    cin >> staffOption;
+    switch (staffOption) {
+        case '1': {
+            int amount = 0;
+            STUDENT *ListOfStudents = new STUDENT[amount];
+            bool ans = loaddata("D:/resoures/Project2_18CLC1_Students.csv", ListOfStudents, amount);
+            if (ans) {
+                ans = savedata("D:/resoures/Project2_18CLC1_Students.txt", ListOfStudents, amount);
+                if (ans) {
+                    cout << "Load Project2_18CLC1_Students and save to Project2_18CLC1_Students.txt successfully!"
+                         << endl;
+                } else {
+                    cout << "Can't load data!! Please try again!" << endl;
+                    system("pause");
+                }
+            }
+            staffContinue(id);
+        }
+            break;
+        case '2': {
+
+        }
+            break;
+        case '3': {
+
+        }
+            break;
+        case '4': {
+
+        }
+            break;
+        case '5': {
+
+        }
+            break;
+        case '6': {
+
+        }
+            break;
+        case '7': {
+
+        }
+            break;
+        case '8': {
+            system("cls");
+            secondStaffMenu(id);
+        }
+    }
+}
+
+void staffContinue(string id) {
+    cout << "\nDo you want to continue?(yes/no)";
+    string YesorNo;
+    cin >> YesorNo;
+    if (YesorNo == "yes") {
+        system("cls");
+        staffMenuClass(id);
+    } else {
+        secondStaffMenu(id);
+    }
+}
+
+bool creatDefaultIP(const char *pathInput, const char *pathOutput) {
+    ifstream fileInput;
+    fileInput.open(pathInput);
+    if (!fileInput.is_open())
+        return false;
+    int n;
+    fileInput >> n;
+    int *id = new int[n];
+
+    int *password = new int[n];
+    Date DoB;
+    char temp;
+    for (int i = 0; i < n; i++) {
+        fileInput >> id[i];
+        do {
+            fileInput >> temp;
+        } while (temp != '1' && temp != '0');
+        fileInput.ignore();
+        fileInput >> DoB.day;
+        fileInput.ignore();
+        fileInput >> DoB.month;
+        fileInput.ignore();
+        fileInput >> DoB.year;
+        fileInput.ignore();
+        password[i] = DoB.year * 10000 + DoB.day * 100 + DoB.month;
+        password[i] = (id[i] % 10000) * 10000 + (password[i] % 10000);
+    }
+    fileInput.close();
+
+    ofstream fileOutput;
+    fileOutput.open(pathOutput);
+    if (!fileOutput.is_open())
+        return false;
+    fileOutput << n << endl;
+    for (int i = 0; i < n; i++) {
+        fileOutput << id[i] << "," << password[i] << endl;;
+    }
+    fileOutput.close();
+    return true;
+}
+
+bool checkLoginStudent(string &account, string &password, const char *path) {
+    ifstream fout;
+    fout.open(path);
+    int aos; /*aos: Amount of firstStudentMenu,firstLectureMenu*/
+    string id, pw;
+    fout >> aos;
+    fout.seekg(3);
+    for (int i = 0; i < aos; i++) {
+        getline(fout, id, ',');
+        getline(fout, pw, '\n');
+        if (id == account && pw == password) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void inputIDPassword(string &id, string &password) {
+    cout << "Please enter your id : ";
+    cin >> id;
+    cout << "\nPlease enter your password : ";
+    cin >> password;
+
+    while (id.size() >= 10 || password.size() >= 10 || (id.size() >= 10 && password.size() >= 10)) {
+        system("cls");
+        cout << "Your account or password has a maximum of 9 characters. Please try again!" << endl;
+
+        cout << "Please enter your id : ";
+
+        cin >> id;
+        cout << "\nPlease enter your password : ";
+
+        cin >> password;
+    }
+}
+
+CLASS::CLASS(string classname, int amount) {
+    this->list = new STUDENT[amount];
+    this->classname = classname;
+}
+
+CLASS::CLASS(const CLASS &c) {
+    this->list = c.list;
+    this->classname = c.classname;
+    this->amount = c.amount;
+}
+
+CLASS::~CLASS() {
+    delete[] list;
+    amount = 0;
+    classname = "Default";
+}
+
+string CLASS::getClassname() {
+    return this->classname;
 }
--- Header.h
+++ Header.h
@@ -1,14 +1,69 @@
-#define _CRT_SECURE_NO_WARNINGS
-#ifndef _student_h
-#define _student_h
+#ifndef _Header_H
+#define _Header_H
+
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <conio.h>
+#define STUDENT_PASSWORD "D:/Document/HCMUS_FIT/KI THUAT LAP TRINH/Lab/PROJECT/StudentManager/Project-KTLT-master/Project-KTLT-master/Password_18CLC6_Students.txt"
+#define DATA_STUDENT "D:/Document/HCMUS_FIT/KI THUAT LAP TRINH/Lab/PROJECT/StudentManager/Project-KTLT-master/Project-KTLT-master/18CLC6_Students.txt"
 using namespace std;
-void menu();
-void staff(char id[]);
-void lecture(char id[]);
-void student(char id[]);
+
+struct Date {
+	int day, month, year;
+};
+
+class STUDENT {
+public:
+	int No;
+	int StudentID;
+	string lastname, firstname;
+	bool gender;
+	Date DoB;
+	int index;
+	string password;
+	void output();
+};
+class CLASS
+{
+public:
+    explicit CLASS(string classname = "Default Class",int amount = 1);
+    CLASS(const CLASS &c);
+    ~CLASS();
+    string getClassname();
+private:
+	string classname;
+	int amount;
+	STUDENT *list = new STUDENT[amount];
+};
 
 
-#endif
+//MENU:
+char firstMenu();
+void inputIDPassword(string &id, string &password);
+void secondMenu(char userOption);
+
+char firstStaffMenu(string id);
+void secondStaffMenu(string id);
+
+void staffContinue(string id);
+
+void firstLectureMenu(string id);
+
+
+void firstStudentMenu(string id);
+bool creatDefaultIP(const char *pathInput, const char *pathOutput);
+bool checkLoginStudent(string &account, string &password, const char *path);
+
+
+void systemContinue(string id);
+void staffMenuClass(string id);
+
+bool loaddata(const char *path, STUDENT *&a, int &n);
+
+void init(STUDENT *&dest, STUDENT a, int &n);
+
+bool savedata(const char *path, STUDENT *&a, int n);
+
+
+#endif //MANAGERPROJ_STUDENT_H#pragma once
